c4c277df2260db45ceb9f145f73dd7ff392a1276
Igor Ovcharenko <KramerJohn@ukr.net>
2020-07-21 21:33:05 +0300

    Refactor PersonPage, add error boundary

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,14 +4,16 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorIndicator from '../error-indiactor';
 
 import './app.css';
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        selectedPerson: null,
+        hasError: false,
     }
 
     toggleRandomPlanet = () => {
@@ -22,17 +24,15 @@ export default class App extends Component {
         })
     }
 
-    onPersonSelected = (id) => {
-        this.setState({
-            selectedPerson: id
-        })
-    }
-
     componentDidCatch() {
-
+        this.setState({hasError: true})
     }
 
     render() {
+        if (this.state.hasError) {
+            return <ErrorIndicator/>
+        }
+
         const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
         return (
@@ -45,15 +45,7 @@ export default class App extends Component {
                     onClick={this.toggleRandomPlanet}>
                     Toggle Random Planet
                 </button>
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={this.onPersonSelected}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>
+                <PeoplePage />
             </div>
         );
     }
